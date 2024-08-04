15483ee94a35f46ba43b54bf110f692d8b3becbd
Uzair-Ghafoor <uzairgbgrt@gmail.com>
2024-08-04 13:33:09 +0500

    single product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,10 +5,44 @@ import { formatCurrency } from '@/utils/format';
 import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
 import AddToCart from '@/components/product/AddToCart';
 import ProductRating from '@/components/product/ProductRating';
+import { Breadcrumb } from '@/components/ui/breadcrumb';
 
 const SingleProduct = async ({ params }: { params: { id: string } }) => {
   const product = await fetchSingleProduct(params.id);
-  return <div>page</div>;
+  const { name, image, company, description, price } = product;
+  const dollarsAmount = formatCurrency(price);
+  return (
+    <section>
+      <BreadCrumbs name={product.name} />
+      <div className=' mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
+        {/* IMAGE */}
+        <div className=' relative h-full'>
+          <Image
+            src={image}
+            alt={name}
+            fill
+            priority
+            sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw'
+            className=' w-full rounded object-cover'
+          />
+        </div>
+        {/* PRODUCT DETAILS */}
+        <div>
+          <div className=' flex gap-x-8 items-center'>
+            <h1 className=' capitalize text-3xl font-bold'>{name}</h1>
+            <FavoriteToggleButton productId={params.id} />
+          </div>
+          <ProductRating productId={params.id} />
+          <h4 className=' text-xl mt-2 '>{company}</h4>
+          <p className=' mt-3 text-md bg-muted inline-block p-2 rounded'>
+            {dollarsAmount}
+          </p>
+          <p className=' mt-6 leading-8 text-muted-foreground'>{description}</p>
+          <AddToCart productId={params.id} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SingleProduct;
